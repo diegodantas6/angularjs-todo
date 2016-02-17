2cfe535053eff2e1830bb66cd8a1dbef7df89e25
diegodantas6 <diegodantas6@gmail.com>
2016-02-17 17:49:01 -0200

    ainda esta perdendo a referencia do obj alterado
    precisa melhorar o controller

--- js/cliente.js
+++ js/cliente.js
@@ -6,31 +6,35 @@ angular.module('todoApp', []).controller('ClienteController', function() {
         id: 0,
         nome: '',
         cpf: ''
-    }
+    };
 
     cliente.lista = [
-        {id: 1, nome: 'Jovem 1',cpf: '123'},
-        {id: 2, nome: 'Jovem 2',cpf: '234'},
-        {id: 3, nome: 'Jovem 3',cpf: '345'}
+        {id: 1, nome: 'Jovem 1', cpf: '123'},
+        {id: 2, nome: 'Jovem 2', cpf: '234'},
+        {id: 3, nome: 'Jovem 3', cpf: '345'}
     ];
 
     cliente.salvar = function() {
         
         cliente.lista.push(cliente.jovem);
         
-        cliente.jovem = null;
+        //cliente.jovem = null;
     };
 
     cliente.visualizar = function(jovem) {
 
-        cliente.jovem = jovem;
+        //cliente.jovem = jovem;
+        
+        angular.copy(jovem, cliente.jovem);
         
         $('#btnSalvar').hide();
     };
 
     cliente.alterar = function(jovem) {
         
-        cliente.jovem = jovem;
+        //cliente.jovem = jovem;
+        
+        angular.copy(jovem, cliente.jovem);
         
         $('#btnSalvar').show();
     };
